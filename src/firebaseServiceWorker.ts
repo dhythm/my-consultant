bce09053a1871ed6a18169484b288cf87fbbf813
dhythm <Yuta.Okada.20@gmail.com>
2020-06-08 14:03:46 +0900

    update

--- src/firebaseServiceWorker.ts
+++ src/firebaseServiceWorker.ts
@@ -1,27 +1,23 @@
 export function register() {
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', function () {
-      Notification.requestPermission(function (result) {
-        console.log({ result });
-        if (result !== 'granted') {
-          console.log('');
-        } else {
-          navigator.serviceWorker.register('/firebase-messaging-sw.js').then(
-            function (registration) {
-              console.log(
-                'firebase-messaging-sw.js registration successful with scope: ',
-                registration.scope,
-              );
-            },
-            function (err) {
-              console.log(
-                'firebase-messaging-sw.js registration failed: ',
-                err,
-              );
-            },
-          );
-        }
-      });
+      if (Notification.permission !== 'granted') {
+        Notification.requestPermission(function (result) {
+          console.log({ result });
+        });
+      } else {
+        navigator.serviceWorker.register('/firebase-messaging-sw.js').then(
+          function (registration) {
+            console.log(
+              'firebase-messaging-sw.js registration successful with scope: ',
+              registration.scope,
+            );
+          },
+          function (err) {
+            console.log('firebase-messaging-sw.js registration failed: ', err);
+          },
+        );
+      }
     });
   }
 }
